7a42b05c0c1762247aa73e68e153b6f1a71a8eb6
256QAM <stephen@arnoldtech.me>
2016-04-26 13:59:15 -0400

    Added help

--- Get-Excuse.ps1
+++ Get-Excuse.ps1
@@ -1,4 +1,29 @@
 function Get-Excuse {
+<#
+
+    .SYNOPSIS   
+		Gets a random excuse.
+         
+    .DESCRIPTION   
+		This module grabs a random excuse from the BOFH Excuse Archive. It will return 1 excuse for each
+		array item piped to it. 
+	
+	.PARAMETER Counts
+		The number of excuses to return
+         
+    .NOTES
+		Author: Stephen Arnold
+		
+		Version 0.1
+			Everything.
+  
+    .EXAMPLE
+		(1..10)|Get-Excuse
+		Returns 10 excuses. 
+
+		
+#> 
+
 [CmdletBinding()]
 	param(
 		[Parameter(ValueFromPipeline=$true)]
